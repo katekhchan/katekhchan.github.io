2769f2577fce7f6186de0de3d0120cf1af2a0d4b
katekhchan <47510485+katekhchan@users.noreply.github.com>
2021-06-17 23:21:38 +0100

    push
    overlay container for carousel perfected - still need to build single pop up  moved overlay work to casestudyhtml

---
+++ js/overlay.js
@@ -0,0 +1,17 @@
+
+function on() {
+  document.getElementById("overlay").style.display = "flex";
+}
+
+function off() {
+  document.getElementById("overlay").style.display = "none";
+}
+
+
+function on2() {
+  document.getElementById("overlay-2").style.display = "flex";
+}
+
+function off2() {
+  document.getElementById("overlay-2").style.display = "none";
+}
